ae0e9cb09a44d3eef4564357aa26f6458a6c477d
Andy Yousef <andyy@kth.se>
2016-04-21 13:54:48 +0200

    add_event fix

--- lab2/calendar.cpp
+++ lab2/calendar.cpp
@@ -1,13 +1,14 @@
 #include "calendar.hpp"
 
 using namespace lab2;
+
 template<typename Datetype>
 Calendar<Datetype>::Calendar(Calendar const& src) {
 	this->calendarDate = src.calendarDate;
 }
 template<typename Datetype>
 Calendar<Datetype>::Calendar() {
-	this->calendarDate = Datetype();
+	this->calendarDate = new Datetype();
 }
 
 
@@ -16,7 +17,8 @@ bool Calendar<Datetype>::set_date(int y, int m, int d) {
 	//Datetype * asd = new Datetype(y, m, d);
 	//this->calendarDate = *asd;
 	try {
-		this->calendarDate = Datetype(y, m, d);
+		delete this->calendarDate;
+		this->calendarDate = new Datetype(y, m, d);
 	}
 	catch(...) {
 		return false;
@@ -26,13 +28,37 @@ bool Calendar<Datetype>::set_date(int y, int m, int d) {
 
 template<typename Datetype>
 bool Calendar<Datetype>::remove_event(int y, int m, int d) {
+	return false;
+}
 
-	template<typename Datetype>
-bool Calendar<Datetype>::add_event(int y, int m, int d) {
+
+
+template<typename Datetype>
+bool Calendar<Datetype>::add_event(std::string input, int y, int m, int d) {
+	Datetype *date = new Datetype(y,m,d);
+	int index = -1;
+	// check if date is in vector
+
+	for(int i=0;i<eventDate.size();++i) {
+		if(*date == eventDate[i]) {
+			delete date;
+			return false;
+		}
+		index = i;
+	}
+	event.push_back(input);
+	eventDate.push_back(*date);
+	std::cout << event[0] << eventDate[0] << std::endl;
+
+	
+	return true;
+}
 
 int main(){
 	Calendar<Gregorian> cal;
 	bool asd;
-	asd = cal.set_date(2000, 14, 11);
-	std::cout << asd << " " << cal.calendarDate << std::endl;
+	asd = cal.set_date(2000, 10, 11);
+	asd = cal.add_event("yasdo", 2000, 10, 11);
+	asd = cal.add_event("yo", 2000, 10, 11);
+	std::cout << asd << " " << *cal.calendarDate << std::endl;
 }
--- lab2/calendar.hpp
+++ lab2/calendar.hpp
@@ -1,12 +1,27 @@
 #include "julian.hpp"
 #include "gregorian.hpp"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#ifndef CALENDAR_HPP
+#define CALENDAR_HPP
+
 
 namespace lab2 {
 	template<typename Datetype> class Calendar {
+	private:
+		bool test();
 	public:
-		Datetype calendarDate;
+		Datetype * calendarDate;
 		bool set_date(int y, int m, int d);
+		bool add_event(std::string input, int y, int m, int d);
+		bool remove_event(int y, int m, int d);
 		Calendar<Datetype>(Calendar const& src);
 		Calendar<Datetype>();
+		std::vector<std::string> event; // definition
+		std::vector<Datetype> eventDate; // definition
+		
 	};
 }
+#endif
